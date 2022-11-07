9e16608105971893cc50453b0c783d0484a72f13
Narendra <narendra.negara@glovory.com>
2022-11-07 10:21:14 +0700

    [ADD] About Section

---
+++ components/layouts/About.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Image from "next/image";
+import { motion } from 'framer-motion'
+
+import ProfileImage from '../../public/images/profile.jpg'
+
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+    <motion.div
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration:1.5}}
+      className='relative flex flex-col h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
+    >
+      <h3 className='absolute top-24 uppercase tracking-[15px] text-2xl'>About</h3>
+      
+      <motion.div
+        className='w-56 h-56 rounded-full object-cover flex-shrink-0 md:rounded-xl md:w-64 md:h-96'
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+        }}
+        viewport={{
+          once: true,
+        }}
+      >
+        <Image
+          className='w-56 h-56 rounded-full object-cover flex-shrink-0 md:rounded-xl md:w-64 md:h-96'
+          src={ProfileImage}
+          alt='about-image'
+        />
+      </motion.div>
+      
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Just a little background
+        </h4>
+        <p>
+        Experienced Web Developer with a demonstrated history of working in the technology industry. Skilled in JavaScript, Typescript, React.js, Next.js, Vue.js, Nuxt.js, Laravel, Sass, Git.
+        Still learning for AngularJS, and other frontend technology.
+        
+        Strong engineering professional with a Bachelor of Technology focused in Information System from Institute Technology Sepuluh Nopember (ITS) Surabaya.
+        </p>
+      </div>
+    </motion.div>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,27 @@
 import Head from 'next/head'
-import Header from '../components/layouts/Header'
-import Hero from '../components/layouts/Hero'
+import Header from '@/components/layouts/Header'
+import Hero from '@/components/layouts/Hero'
+import About from '@/components/layouts/About'
 
 export default function Home() {
   return (
     <div className='bg-[#2C3333] text-[#E7F6F2] h-screen snap-y snap-mandatory overflow-y-scroll z-0'>
       <Head>
-        <title>Negara Portofolio</title>
+        <title>Narendra Portofolio</title>
       </Head>
       
       {/* Header */}
       <Header />
       
       {/* Hero */}
-      <section id='hero' className=''>
+      <section id='hero' className='snap-start'>
         <Hero />
       </section>
       
       {/* About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
       
       {/* Experience */}
       
