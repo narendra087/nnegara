76cdaa636c4e9ea21366bcdec4ba5e3ff0035e16
Narendra <personal.rendra@gmail.com>
2023-01-25 18:19:54 +0700

    [UPD] Experience

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Image from "next/image";
 import { motion } from 'framer-motion'
 
-import glovory from '../public/images/glovory.jpeg'
+type Props = {
+  expData: any,
+}
 
-type Props = {}
-
-const ExperienceCard = (props: Props) => {
+const ExperienceCard = ({expData}: Props) => {
   return (
     <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[calc(100vw-150px)] md:w-[500px] xl:w-[600px] snap-center bg-[#395B64]/50 p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-300 overflow-hidden'>
       <motion.div
@@ -20,31 +20,38 @@ const ExperienceCard = (props: Props) => {
       >
         <Image
           className='w-32 h-32 rounded-full object-cover object-center'
-          src={glovory}
+          src={expData.image}
           alt='about-image'
         />
       </motion.div>
       
       <div className='px-0'>
-        <h4 className='text-4xl font-light'>Frontend Developer</h4>
-        <p className='font-bold text-2xl mt-1 '>Glovory</p>
+        <h4 className='text-4xl font-light'>{expData.title}</h4>
+        <p className='font-bold text-2xl mt-1 '>{expData.work}</p>
         <div className='flex space-x-2 my-2'>
-          <Image
-            className='h-10 w-10 rounded-full'
-            src={glovory}
-            alt=''
-          />
-          <Image
-            className='h-10 w-10 rounded-full'
-            src={glovory}
-            alt=''
-          />
+          {
+            expData.tools.map((tool:any, index:number) => {
+              return (
+                <Image
+                  key={index}
+                  title={tool.title}
+                  className='h-10 w-10 min-w-[2.5rem] rounded-full object-cover'
+                  src={tool.image}
+                  alt=''
+                />
+              )
+            })
+          }
         </div>
-        <p className='uppercase py-5'>Start date</p>
+        <p className='uppercase py-5'>{expData.startDate} - {expData.endDate}</p>
         <ul className='list-disc space-y-4 ml-5 text-lg'>
-          <li>Job Description</li>
-          <li>Job Description</li>
-          <li>Job Description</li>
+          {
+            expData.jobDesc.map((job:any, index:number) => {
+              return (
+                <li key={index}>{job}</li>
+              )
+            })
+          }
         </ul>
       </div>
     </article>
--- components/layouts/WorkExperience.tsx
+++ components/layouts/WorkExperience.tsx
@@ -2,9 +2,105 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import ExperienceCard from '../ExperienceCard'
 
+import glovory from '../../public/images/glovory.jpeg'
+import onlist from '../../public/images/onlist.jpeg'
+
+import remix from '../../public/images/remix.jpeg'
+import refine from '../../public/images/refine.jpeg'
+import next from '../../public/images/next.jpeg'
+import react from '../../public/images/react.jpeg'
+import ts from '../../public/images/ts.jpeg'
+import sass from '../../public/images/sass.jpeg'
+import angular from '../../public/images/angular.jpeg'
+
+import vue from '../../public/images/vue.jpeg'
+import nuxt from '../../public/images/nuxt.jpeg'
+import js from '../../public/images/js.jpeg'
+
 type Props = {}
 
 const WorkExperience = (props: Props) => {
+  const experienceList = [
+    {
+      image: glovory,
+      work: 'Glovory',
+      title: 'Frontend Developer',
+      tools: [
+        {
+          image: remix,
+          title: 'Remix',
+          link: 'https://remix.run/'
+        },
+        {
+          image: refine,
+          title: 'Refine',
+          link: 'https://refine.dev/'
+        },
+        {
+          image: next,
+          title: 'NextJS',
+          link: 'https://nextjs.org/'
+        },
+        {
+          image: react,
+          title: 'ReactJS',
+          link: 'https://reactjs.org/'
+        },
+        {
+          image: angular,
+          title: 'AngularJS',
+          link: 'https://angular.io/'
+        },
+        {
+          image: ts,
+          title: 'TypeScript',
+          link: 'https://typescriptlang.org/'
+        },
+        {
+          image: sass,
+          title: 'SASS',
+          link: 'https://sass-lang.com/'
+        }
+      ],
+      startDate: 'Feb, 07 2022',
+      endDate: 'Present',
+      jobDesc: [
+        'Develop Hotel Management System, using Refine and Remix (ReactJS)',
+        'Bug fixing "Special Education IMS (Himmaty) project, using AngularJS',
+      ]
+    },
+    {
+      image: onlist,
+      work: 'OnList',
+      title: 'Frontend Developer',
+      tools: [
+        {
+          image: nuxt,
+          title: 'NuxtJS',
+          link: 'https://nuxtjs.org/'
+        },
+        {
+          image: vue,
+          title: 'VueJS',
+          link: 'https://vuejs.org/'
+        },
+        {
+          image: js,
+          title: 'JavaScript',
+          link: 'https://javascript.com/'
+        },
+      ],
+      startDate: 'Jan, 01 2020',
+      endDate: 'Feb, 06 2022',
+      jobDesc: [
+        'Develop numerous features, such as forum page, blog page, google adsense integration, and many more internal features using NuxtJS',
+        'Build Content Management System (CMS) for onlist.id using VueJS',
+        'Handle process of increasing the ranking and visibility of Onlist Website (SEO)',
+        'Implementing best practices and website performance seen from google search console. Managed to increase performance around 90%'
+      ]
+    }
+  ]
+  
   return (
     <motion.div
       initial={{opacity:0}}
@@ -15,9 +111,13 @@ const WorkExperience = (props: Props) => {
       <h3 className='absolute top-24 uppercase tracking-[15px] text-2xl'>Experience</h3>
       
       <div className='w-full flex space-x-5 overflow-x-scroll snap-x snap-mandatory pb-5'>
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+        {
+          experienceList.map((exp:any, index: number) => {
+            return (
+              <ExperienceCard key={index} expData={exp} />
+            )
+          })
+        }
       </div>
     </motion.div>
   )
