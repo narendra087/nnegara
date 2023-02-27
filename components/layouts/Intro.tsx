b4f8783ebd1a142f25a44d4116a6648a9bcaf881
Narendra <personal.rendra@gmail.com>
2023-02-27 15:14:07 +0700

    [ADD] Babel inline svg

--- components/layouts/Intro.tsx
+++ components/layouts/Intro.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { motion } from 'framer-motion'
 
 import ProfileImage from '../../public/images/profile-intro.png'
+import Code from '../../public/icons/code.svg'
 
 const Intro = () => {
   return (
@@ -10,7 +11,7 @@ const Intro = () => {
       <div className='picture-section flex-1 relative'>
         <motion.div
           
-          className='circle circle-orange-2 w-[400px] h-[400px] absolute top-1/3 left-10 bg-[#ECDBBA] rounded-[30%] rotate-[30deg]'></motion.div>
+          className='circle circle-orange-2 w-[380px] h-[380px] absolute top-1/3 left-10 bg-[#ECDBBA] rounded-[25%] rotate-[30deg]'></motion.div>
         <motion.div
           initial={{
             // x: -500,
@@ -32,16 +33,20 @@ const Intro = () => {
         </motion.div>
       </div>
       <div className="flex flex-col flex-1 pt-20 relative">
-        <h1 className='text-[60px]'>Hi, I Am</h1>
-        <h2 className='text-[40px] text-transparent bg-clip-text bg-gradient-to-tr from-[#C84B31] to-[#ECDBBA]'>Narendra Puspa Adi Negara</h2>
+        <h1 className='text-[50px]'>Hi, I Am</h1>
+        <h2 className='text-[35px] text-transparent bg-clip-text bg-gradient-to-tr from-[#C84B31] to-[#ECDBBA]'>Narendra Puspa Adi Negara</h2>
         <p>
           <span className='underline underline-offset-4 decoration-[#C84B31]'>Frontend Developer</span> with 3+ years experience in technology industry.<br /> Deliver <span className='underline underline-offset-4 decoration-[#C84B31]'>clean code</span> and producing the <span className='underline underline-offset-4 decoration-[#C84B31]'>good product</span>.
         </p>
         <nav className='flex gap-4 mt-6'>
-          <a className='hover:text-[#C84B31] transition-colors duration-300' href="#about">About</a>
-          <a className='hover:text-[#C84B31] transition-colors duration-300' href="#experience">Experience</a>
-          <a className='hover:text-[#C84B31] transition-colors duration-300' href="#portfolio">Portfolio</a>
+          <a className='hover:text-[#C84B31] transition-colors duration-300 py-2 px-4 bg-[#ECDBBA] text-[#191919] rounded-xl' href="#about">About</a>
+          <a className='hover:text-[#C84B31] transition-colors duration-300 py-2 px-4 bg-[#ECDBBA] text-[#191919] rounded-xl' href="#experience">Experience</a>
+          <a className='hover:text-[#C84B31] transition-colors duration-300 py-2 px-4 bg-[#ECDBBA] text-[#191919] rounded-xl' href="#portfolio">Portfolio</a>
         </nav>
+        <div className='absolute bottom-0 right-0 text-[200px] text-[#C84B31] opacity-60'>
+          {/* <Code /> */}
+          {"</>"}
+        </div>
       </div>
     </div>
   )
