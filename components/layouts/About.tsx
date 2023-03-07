7e6ce5198dae52cf696392a5c5dc53ea3196ebad
Narendra <personal.rendra@gmail.com>
2023-03-07 10:06:40 +0700

    [UPD] About section

--- components/layouts/About.tsx
+++ components/layouts/About.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Image from "next/image";
+import CountUp from 'react-countup';
 import { motion } from 'framer-motion'
 
-import ProfileImage from '../../public/images/bg.jpg'
+import ProfileImage from '../../public/images/bg02.jpg'
 
 type Props = {}
 
@@ -16,7 +17,7 @@ const About = (props: Props) => {
     >
       <h3 className='uppercase tracking-[15px] relative left-[15px] md:text-2xl text-xl'>About Me</h3>
       
-      <div className='flex flex-col md:flex-row gap-5 py-5 md:py-0 my-auto items-center relative'>
+      <div className='flex flex-col md:flex-row-reverse gap-5 py-5 md:py-0 my-auto items-center relative max-w-4xl'>
         <motion.div
           className='w-48 h-48 rounded-full object-cover flex-shrink-0 md:rounded-xl md:w-64 md:h-96 relative z-10'
           initial={{
@@ -35,23 +36,32 @@ const About = (props: Props) => {
           }}
         >
           <Image
-            className='w-48 h-48 rounded-full object-cover flex-shrink-0 md:rounded-xl md:w-64 md:h-96'
+            className='w-48 h-48 rounded-full object-cover flex-shrink-0 md:rounded-xl md:w-auto md:h-auto object-bottom md:object-none'
             src={ProfileImage}
             alt='about-image'
           />
         </motion.div>
         <motion.div
-          className='circle z-0 circle-orange-2 lg:w-[500px] lg:h-[500px] w-[400px] h-[400px] transition-all duration-300 absolute  md:bg-[#ECDBBA] bg-[#191919] rounded-[25%] rotate-[30deg] -bottom-[250px] -left-[250px]'>
+          className='circle z-0 circle-orange-2 lg:w-[500px] lg:h-[500px] w-[400px] h-[400px] transition-all duration-300 absolute  md:bg-[#191919] bg-[#191919] rounded-[25%] rotate-[30deg] -bottom-[250px] -left-[250px] lg:-left-[400px]'>
         </motion.div>
         
         <div className="px-0 md:px-5 flex flex-col gap-4 relative z-10">
-          <h4 className="lg:text-4xl md:text-3xl text-xl font-semibold text-[#C84B31]">
-            Just a little background
+          <h4 className="md:text-3xl text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-tr from-[#C84B31] to-[#ECDBBA]">
+            A Front-end Developer with over 3 years of experience.
           </h4>
           <p className='md:text-md text-sm'>
-            A <span className='text-[#C84B31] underline underline-offset-4 decoration-[#C84B31]'>Front End Developer</span> with 3+ years of experience working in the technology industry. Familiar with web developer skills such as JavaScript, NuxtJS, VueJS, Vuex, NextJS, ReactJS, Sass, TailwindCSS, HTML5, CSS, Laravel, CodeIgniter and still learning for AngularJS, Refine and Remix.
+            Familiar with web developer skills and tools such as JavaScript, TypeScript, NuxtJS, VueJS, Vuex, NextJS, ReactJS, Redux, Sass, TailwindCSS, HTML, CSS, Laravel, CodeIgniter and still learning for AngularJS, Refine and Remix.
           </p>
-          <p className='md:text-md text-sm'>I am interested in front end development, and want to learn about front end technology that I have never mastered.</p>
+          <div className="flex items-center gap-5 mx-auto md:mx-0">
+            <div className="flex flex-col gap-2 max-w-[80px]">
+              <CountUp end={3} suffix="+" className="md:text-5xl text-4xl text-transparent bg-clip-text bg-gradient-to-tr from-[#C84B31] to-[#ECDBBA]" />
+              <p className="text-sm">Years of Experience</p>
+            </div>
+            <div className="flex flex-col gap-2 max-w-[80px]">
+              <CountUp end={4} className="md:text-5xl text-4xl text-transparent bg-clip-text bg-gradient-to-tr from-[#C84B31] to-[#ECDBBA]" />
+              <p className="text-sm">Projects Completed</p>
+            </div>
+          </div>
         </div>
       </div>
     </motion.div>
