2f2f113f4ec243911e41b8b6991c5b7cd701ace8
Narendra <personal.rendra@gmail.com>
2023-03-08 00:56:42 +0700

    [FIX] Experience glitch

--- components/layouts/Experience.tsx
+++ components/layouts/Experience.tsx
@@ -75,7 +75,7 @@ const Experience = () => {
   }
   
   return (
-    <div className='section md:pt-[90px] pt-[70px] px-5 xl:mx-auto mx-5 max-w-7xl bg-[#191919] transition-all duration-300'>
+    <div className='section md:pt-[90px] pt-[70px] px-5 xl:mx-auto mx-5 max-w-7xl bg-[#191919] transition-all duration-300 overflow-hidden'>
       <div className="md:min-h-[calc(100vh-90px)] min-h-[calc(100vh-58px)] max-w-4xl mx-auto">
         <h3 className='uppercase tracking-[15px] relative left-[15px] md:text-2xl text-xl text-center'>Experience</h3>
         <div className="flex justify-center mt-10 lg:pb-20 pb-16">
