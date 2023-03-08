db9c6d7b20dec44e090f89153feca363b75dbf08
Narendra <personal.rendra@gmail.com>
2023-03-09 02:01:01 +0700

    [UPD] Project section

--- components/layouts/Project.tsx
+++ components/layouts/Project.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import { useState } from 'react'
 
 import bg from '../../public/images/project-bg.png'
 import Onlist01 from '../../public/images/project-onlist-01.png'
@@ -10,33 +9,41 @@ import NersCov from '../../public/images/project-ners.png'
 import Cilacs from '../../public/images/project-cilacs.png'
 
 const Project = () => {
-  const [showHidden, setShow] = useState(false)
-  
-  const projectCard = (image: any, job: String, title: String, desc: String = '', hidden: Boolean = false, comingSoon: Boolean = false) => {
+  const projectCard = (
+    image: any,
+    job: String,
+    title: String,
+    desc: String = '',
+    comingSoon: Boolean = false,
+    onlyDesktop: Boolean = false,
+    onlyMobile: Boolean = false
+  ) => {
     return (
-      <div className={`group relative overflow-hidden border-2 border-white/50 rounded-xl ${hidden && !showHidden ? 'hidden' : ''}`}>
+      <div
+        className={`group relative overflow-hidden border-2 border-white/50 rounded-xl
+          ${onlyDesktop ? 'lg:block hidden' : ''} ${onlyMobile ? 'lg:hidden block' : ''}
+        `}
+      >
         <div className="overlay group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
         <Image src={image} alt={`project-${title}`} className='group-hover:scale-125 transition-all duration-500' />
         <div
-          className={`
-            absolute -bottom-full transition-all duration-500 z-50
-            ${comingSoon ? 'left-1/2 -translate-x-1/2 group-hover:bottom-10' : 'left-4 group-hover:bottom-24'}
+          className={`absolute -bottom-full transition-all duration-500 z-50
+            ${comingSoon ? 'left-1/2 -translate-x-1/2 group-hover:lg:bottom-10 group-hover:bottom-4' : 'left-4 group-hover:bottom-24'}
           `}
         >
           <span
-            className={`
-              text-xl font-bold text-gradient
-              ${comingSoon ? 'text-3xl' : 'text-xl'}
+            className={`font-bold text-gradient
+              ${comingSoon ? 'text-2xl' : 'text-xl'}
             `}
           >
             { job }
           </span>
         </div>
-        <div className="absolute -bottom-full left-4 group-hover:bottom-14 group-hover:lg:bottom-14 transition-all duration-700 z-50">
-          <span className='text-3xl text-white'>{ title }</span>
+        <div className="absolute -bottom-full left-4 group-hover:bottom-[60px] group-hover:lg:bottom-14 transition-all duration-700 z-50">
+          <span className='md:text-3xl text-2xl text-white'>{ title }</span>
         </div>
-        <div className="absolute -bottom-full left-4 group-hover:bottom-6 group-hover:lg:bottom-2 transition-all duration-700 z-50 max-w-sm">
-          <p>{ desc }</p>
+        <div className="absolute -bottom-full left-4 group-hover:bottom-4 group-hover:lg:bottom-2 transition-all duration-700 z-50 max-w-sm">
+          <p className='md:text-base text-sm'>{ desc }</p>
         </div>
         {
           comingSoon &&
@@ -53,11 +60,11 @@ const Project = () => {
           <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
             <div className='flex-1 flex flex-col'>
               <h3 className='text-gradient uppercase font-bold text-3xl leading-tight mb-5'>My latest<br /> Work</h3>
-              <p className='max-w-sm mb-16'>
+              <p className='max-w-sm mb-9'>
                 This is my latest work including individual project and professional project.
-                There are still ongoing project, You can show the hidden project by clicking the button below.
+                I&apos;m open to become a freelancer or be one of your developer team, if you are interested in hiring me, click the button below.
               </p>
-              <button className={`btn w-fit ${showHidden ? 'btn-secondary' : 'btn-primary'}`} onClick={() => setShow(!showHidden)}>{showHidden ? 'Hide ongoing projects' : 'Show ongoing projects'}</button>
+              <button className={`btn w-fit btn-primary`}>Hire Me</button>
             </div>
             { projectCard(Onlist01, 'Frontend Developer', 'OnList', 'An online marketplace for selling and renting residence.') }
             { projectCard(Onlist02, 'Frontend Developer', 'OnList CMS', 'Content Management System (CMS) for OnList Website') }
@@ -67,7 +74,8 @@ const Project = () => {
             { projectCard(Tracer, 'Fullstack Developer', 'Tracer Study ITS', 'An online survey for Institut Teknologi Sepuluh Nopember Alumni.') }
             { projectCard(NersCov, 'Fullstack Developer', 'NersCov', 'An online screening Covid-19 assessment in Surabaya.') }
             { projectCard(Cilacs, 'Frontend Developer', 'Cilacs UII', 'An online test center and course registration from Cilacs UII.') }
-            { projectCard(bg, 'Stay tune!', '', '', true, true) }
+            { projectCard(bg, 'Stay tune!', '', '', true) }
+            { projectCard(bg, 'Stay tune!', '', '', true, false, true) }
           </div>
         </div>
       </div>
